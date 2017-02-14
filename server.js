e7d71b96dcbde29083389209079983578451f135
SandeepBanda <sandeep.banda94@gmail.com>
2017-02-14 22:47:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ app.get('/article-one',function(req,res)
     res.send('Hi this is article one');
 });
 
+app.get('/article-two',function(req,res)
+{
+    res.sendFile(path.join(_dirname,'ui','articletwo.html'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
