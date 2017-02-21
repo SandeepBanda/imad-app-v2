45906c56a3dac80461559adc0308a62e38dd439d
SandeepBanda <sandeep.banda94@gmail.com>
2017-02-21 19:06:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,10 @@ console.log('Loaded!');
 var element1 = document.getElementById("id2");
 
 element1.innerHTML = "Assigning";
+
+var element2 = document.getElementById("id1");
+
+element.oncilck = function()
+{
+    element.style.marginLeft = "100px";
+};
