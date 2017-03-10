c07253a66cf68ee39deaa5ce228f9bedfcc78a23
SandeepBanda <sandeep.banda94@gmail.com>
2017-03-10 10:19:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,14 +8,12 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-one',function(req,res)
-{
-    res.send('Hi this is article one');
-});
 
-app.get('/article-two',function(req,res)
-{
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+
+var counter=0;
+app.get('/counter', function (req, res) {
+  counter=counter+1;
+  res.send(counter.toString());
 });
 
 app.get('/ui/style.css', function (req, res) {
@@ -30,8 +28,99 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
+var namelist =[];
+app.get('/submit-name',function(req,res)
+{
+
+	// Gettig Name
+
+	var name = req.query.name;
+
+	// Pushing into list
+
+	namelist.push(name);
+
+	// Displaying the data
+
+	res.send(JSON.stringify(namelist));
+
+});
+
+var comments = [];
+app.get('/comments',function(req,res)
+{
+	var name = req.query.name;
+	var email = req.query.email;
+	var comment = req.query.comment;
+
+	comments.push(name);
+	comments.push(email);
+	comments.push(comment);
+
+    res.send(JSON.stringify(comments));
+
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
+
+var articleOne = { 
+	title : 'This is Article One Testing',
+	heading : 'Article',
+	date : 'Sep 27,2016',
+	content : `
+		<p>This is first line</p>
+		</div>
+
+		<div>
+		<p>This is second line</p>
+		</div>
+
+		<div>
+		<p>This is third line</p>`
+};
+
+app.get('/article-one',function(req,res){
+	res.send(createHTMLTemplate(articleOne));
+});
+
+function createHTMLTemplate(data)
+{
+	var title = data.title;
+	var heading = data.heading;
+	var date = data.date;
+	var content = data.content;
+
+	var HTMLTemplate = `
+			<!DOCTYPE html>
+			<html>
+			<head>
+			<title>
+			${title}
+			</title>
+			<link rel="stylesheet" type="text/css" href="/ui/style.css ">
+			</head>
+			<body>
+			<div class="container">
+			<div>
+			<p>
+			${heading}
+			</p>
+			</div>
+
+			<div>
+			<p>
+			${date}
+			</p>
+			</div>
+
+			<div>
+			${content}
+			</div>
+			</body>
+			</html>
+`;
+	return HTMLTemplate;
+}
